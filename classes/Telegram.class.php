387a9450d29c1f915d39ad0aa419554923f956c0
sgilyin <public.sgilyin@gmail.com>
2021-10-15 13:50:43 +0500

    ТГ: отправка сообщений об ошибках

--- classes/Telegram.class.php
+++ classes/Telegram.class.php
@@ -8,12 +8,16 @@
 class Telegram {
     public static function sendMessage($login, $args) {
         if (TELEGRAM_ENABLED && TELEGRAM_BOT_TOKEN != '') {
-            $url = 'https://api.telegram.org/bot' . TELEGRAM_BOT_TOKEN . '/' . __FUNCTION__;
-            return cURL::executeRequestTest('POST', $url, $args, false, false);
+            if (isset($args['chat_id']) && isset($args['text'])) {
+                Logs::handler(__CLASS__."::".__FUNCTION__." | $login | " . serialize($args));
+                $url = 'https://api.telegram.org/bot' . TELEGRAM_BOT_TOKEN . '/' . __FUNCTION__;
+                return cURL::executeRequestTest('POST', $url, $args, false, false);
+            }
         }
     }
 
     public static function alert($message) {
+        Logs::handler(__CLASS__."::".__FUNCTION__." | $message");
         global $TgAlrtDst;
         for ($index = 0; $index < count($TgAlrtDst); $index++) {
             $args = array(
