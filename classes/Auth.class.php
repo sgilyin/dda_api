ad4ee7f0a679c0ee5749fec6cffd9d4af70da695
sgilyin <public.sgilyin@gmail.com>
2022-05-17 09:40:52 +0500

    Auth: рефактор, проверка доступа через БД

--- classes/Auth.class.php
+++ classes/Auth.class.php
@@ -5,23 +5,20 @@
  * @author Sergey Ilyin <developer@ilyins.ru>
  */
 class Auth {
-    public static function logIn($login, $inputRequestData) {
-        if (isset($inputRequestData)) {
-            if (isset($inputRequestData['user']) && isset($inputRequestData['password'])) {
+    public static function logIn($login, $args) {
+        if (isset($args)) {
+            if (isset($args['user']) && isset($args['password'])) {
                 echo <<<HTML
                 <form method=post>
-                    User: <input type=text id=user name=user value=$inputRequestData[user]>
-                    Password: <input type=password id=password name=password value=$inputRequestData[password]>
+                    User: <input type=text id=user name=user value=$args[user]>
+                    Password: <input type=password id=password name=password value=$args[password]>
                     <input type=submit value=LogIn>
                 </form>
                 HTML;
-                $query = "SELECT user, password, status, salt FROM gc_managers WHERE login='$login' AND user='{$inputRequestData['user']}'";
-                $manager = DB::query($query)->fetch_object();
-                if (md5(md5($inputRequestData['password']).$manager->salt) == $manager->password && $manager->status == 'enable') {
-                    DB::getManagersDeals($login, $manager->user);
-                } else {
-                    echo 'Неверный логин/пароль или пользователь отключен';
-                }
+                $deals = (self::checkAccess($login, $args)) ?
+                    DB::getManagersDeals($login, $args['user']) :
+                    'Неверный логин/пароль или пользователь отключен';
+                echo $deals;
             }
         } else {
             echo <<<HTML
@@ -32,5 +29,13 @@ class Auth {
             </form>
             HTML;
         }
+    }
+
+    public static function checkAccess($login, $args) {
+        $query = "SELECT user, password, status, salt FROM gc_managers WHERE login='$login' AND user='{$args['user']}'";
+        $manager = DB::query($query)->fetch_object();
+        $access = (md5(md5($args['password']).$manager->salt) == $manager->password && $manager->status == 'enable') ? true : false;
+        Logs::handler(__CLASS__.'::'.__FUNCTION__." | $login | {$args['user']} | $access");
+        return $access;
     }
 }
