17d65fd827cd8143634b16041659d59d3153fd08
sgilyin <developer@ilyins.ru>
2022-08-25 16:57:29 +0500

    Куча правок

--- classes/SemySMS.class.php
+++ classes/SemySMS.class.php
@@ -8,77 +8,64 @@
 class SemySMS {
     public static function trap($login, $inputRequestData) {
         if (SEMYSMS_ENABLED) {
-            if ($inputRequestData['type']=='2') {
-                Logs::handler(__CLASS__ . '::' . __FUNCTION__ . 
-                    ' | RCVD | ' . $inputRequestData['phone'] . 
-                    ' | ' . $inputRequestData['msg']);
-                $phone = substr(preg_replace('/[^0-9]/', '', $inputRequestData['phone']), -15);
-                try {
-                    $obj = DB::query("SELECT email FROM gc_users WHERE login='$login' AND phone REGEXP '$phone'")->fetch_object();
-                    if (isset($obj->email)) {
-                        $email = $obj->email;
+            Logs::handler(sprintf('%s::$s | $s', __CLASS__, __FUNCTION__,
+                $inputRequestData['phone'], $inputRequestData['msg']));
+            switch ($inputRequestData['type']) {
+                case'':
+                    $argsI['message'] = sprintf('Входящее от %s (%s): %s',
+                        $inputRequestData['phone'], $inputRequestData['date'],
+                        $inputRequestData['msg']);
+                    Wazzup24::alertSemySMS($login, $argsI);
+                    break;
+                case '0':
+                    $whatsapp['to'] = WA_SEMYSMS_NOTIFY;
+                    $whatsapp['transport'] = 'whatsapp';
+                    $whatsapp['text'] = $inputRequestData['phone'] . PHP_EOL .
+                        $inputRequestData['msg'] . PHP_EOL . $inputRequestData['date'];
+                    Wazzup24::queue($login, $whatsapp);
+                    $toChatApi['phone'] = WA_SEMYSMS_NOTIFY;
+                    $toChatApi['body'] = $inputRequestData['phone'] . PHP_EOL .
+                        $inputRequestData['msg'] . PHP_EOL . $inputRequestData['date'];
+                    ChatApi::queue($login, $toChatApi);
+                    break;
+                case'2':
+                    $phone = substr(preg_replace('/[^0-9]/', '', $inputRequestData['phone']), -15);
+                    try {
+                        $user = DB::query("SELECT email FROM gc_users WHERE login='$login' AND phone REGEXP '$phone'")->fetch_object();
+                        !isset($user->email) ?: $email = $user->email;
+                    } catch (Exception $exc) {
+                        Logs::error(sprintf('%s::%s | var user | %s', __CLASS__,
+                            __FUNCTION__, $exc));
                     }
-                } catch (Exception $exc) {
-                    #nothing yet
-                }
-                if (!isset($email)) {
                     preg_match("/\|.*\|/",$inputRequestData['msg'],$matches);
                     if ($matches) {
                         $item=explode("|", $matches[0]);
-                        if ($item[1]) {
-                            global $addFields;
-                            $params['user']['group_name']= array($addFields->{$item[1]});
-                        }
-                        if ($item[2]) {
-                            $params['user']['addfields']['d_utm_source']=$item[2];
-                        }
-                        if ($item[3]) {
-                            $params['user']['addfields']['d_utm_medium']=$item[3];
-                        }
-                        if ($item[4]) {
-                            $params['user']['addfields']['d_utm_content']=$item[4];
-                        }
-                        if ($item[5]) {
-                            $params['user']['addfields']['d_utm_campaign']=$item[5];
-                        }
-                        if ($item[6]) {
-                            $params['user']['addfields']['d_utm_term']=$item[6];
-                        }
-                        if ($item[7]) {
-                            $params['user']['addfields']['d_utm_rs']=$item[7];
-                        }
-                        if ($item[8]) {
-                            $params['user']['addfields']['d_utm_acc']=$item[8];
-                        }
-                        if ($item[9]) {
-                            $params['user']['addfields']['Возраст']=$item[9];
-                        }
-                        if ($item[10]) {
-                            $emailInMessage=$item[10];
-                        }
+                        global $addFields;
+                        !isset($item[1]) ?: $params['user']['group_name']= array($addFields->{$item[1]});
+                        !isset($item[2]) ?: $params['user']['addfields']['d_utm_source']=$item[2];
+                        !isset($item[3]) ?: $params['user']['addfields']['d_utm_medium']=$item[3];
+                        !isset($item[4]) ?: $params['user']['addfields']['d_utm_content']=$item[4];
+                        !isset($item[5]) ?: $params['user']['addfields']['d_utm_campaign']=$item[5];
+                        !isset($item[6]) ?: $params['user']['addfields']['d_utm_term']=$item[6];
+                        !isset($item[7]) ?: $params['user']['addfields']['d_utm_rs']=$item[7];
+                        !isset($item[8]) ?: $params['user']['addfields']['d_utm_acc']=$item[8];
+                        !isset($item[9]) ?: $params['user']['addfields']['Возраст']=$item[9];
+                        !isset($item[10]) ?: $emailInMessage=$item[10];
                     }
-                    $email = $emailInMessage ?? "$phone@facebook.com";
-                }
-                $params['user']['phone'] = $phone;
-                $params['user']['email'] = $email;
-                $params['user']['addfields']['whatsapp']=$phone;
-                GetCourse::usersAdd($login, $params);
-                $alreadySent = DB::checkSentGetCourse($email, $inputRequestData['msg']);
-                if (!$alreadySent) {
-                    DB::query("INSERT INTO gc_contact_form SET email='$email', text='{$inputRequestData['msg']}'");
-                    GetCourse::sendContactForm($email, $inputRequestData['msg'].PHP_EOL.'Отправлено из WhatsApp ('.__CLASS__.')');
-                }
-            }
-            if ($inputRequestData['type']=='0') {
-                $whatsapp['to'] = WA_SEMYSMS_NOTIFY;
-                $whatsapp['transport'] = 'whatsapp';
-                $whatsapp['text'] = $inputRequestData['phone'] . PHP_EOL .
-                    $inputRequestData['msg'] . PHP_EOL . $inputRequestData['date'];
-                Wazzup24::queue($login, $whatsapp);
-                $toChatApi['phone'] = WA_SEMYSMS_NOTIFY;
-                $toChatApi['body'] = $inputRequestData['phone'] . PHP_EOL .
-                    $inputRequestData['msg'] . PHP_EOL . $inputRequestData['date'];
-                ChatApi::queue($login, $toChatApi);
+                    $email = $email ?? $emailInMessage ?? "$phone@facebook.com";
+                    $params['user']['phone'] = $phone;
+                    $params['user']['email'] = $email;
+                    $params['user']['addfields']['whatsapp']=$phone;
+                    GetCourse::usersAdd($login, $params);
+                    $alreadySent = DB::checkSentGetCourse($email, $inputRequestData['msg']);
+                    if (!$alreadySent) {
+                        DB::query("INSERT INTO gc_contact_form SET email='$email', text='{$inputRequestData['msg']}'");
+                        GetCourse::sendContactForm($email, $inputRequestData['msg'].PHP_EOL.'Отправлено из WhatsApp ('.__CLASS__.')');
+                    }
+                    break;
+
+                default:
+                    break;
             }
         }
     }
