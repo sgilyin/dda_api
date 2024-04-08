164691ea2bf9f8a2572e310314ddaa8e40d7b3a4
sgilyin <developer@ilyins.ru>
2024-04-08 22:59:29 +0500

    Интеграция с КлаудКассир

---
+++ classes/CloudKassir.class.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * Class for CloudKassir
+ *
+ * @author Sergey Ilyin <developer@ilyins.ru>
+ */
+class CloudKassir {
+    private function execute($login, $args) {
+        if (CLOUDKASSIR_ENABLED) {
+            Logs::handler(sprintf('%s::%s | %s | %s', __CLASS__, __FUNCTION__,
+                $login, serialize($args)));
+            $url = CLOUDKASSIR_URL . $args['action'];
+            $headers[] = 'Content-Type: application/json';
+//            $headers[] = 'Content-Length: 0';
+            $userpwd = sprintf('%s:%s', CLOUDKASSIR_PUBLIC_ID, CLOUDKASSIR_API_SECRET);
+            $post = $args['json'];
+            return cURL::executeRequest($url, $post, $headers, $userpwd, false);
+        }
+    }
+
+    public function correctionReceipt($login, $args) {
+        if (CLOUDKASSIR_ENABLED) {
+            Logs::handler(sprintf('%s::%s | %s | %s', __CLASS__, __FUNCTION__,
+                $login, serialize($args)));
+            $cr['action'] = '/kkt/correction-receipt';
+            $json['CorrectionReceiptData'] = array(
+                'OrganizationInn' => CLOUDKASSIR_INN,
+                'VatRate' => 6,
+                'TaxationSystem' => $args['TaxationSystem'],
+                'DeviceNumber' => CLOUDKASSIR_DEVICENUMBER,
+                'CorrectionReceiptType' => 1,
+                'CauseCorrection' => array(
+                    'CorrectionDate' => $args['CorrectionDate'],
+                    'CorrectionNumber' => 'б//н'),
+                'Amounts' => array(
+                    'AdvancePayment' => $args['Amount']),
+                'Items' => array(array(
+                    'Label' => 'Обучающий курс ' . $args['Label'],
+                    'Price' => $args['Amount'],
+                    'Quantity' => 1,
+                    'Vat' => 0,
+                    'Amount' => $args['Amount'])),
+                'PaymentPlace' => CLOUDKASSIR_PAYMENTPLACE,
+                'CorrectionType' => 0);
+            $cr['json'] = json_encode($json);
+//            var_dump(json_encode($json));
+            echo self::execute($login, $cr);
+        }
+    }
+}
